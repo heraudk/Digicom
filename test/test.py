ba7775b4cf31392aad5162ed5b0f7f4b62c03576
heraudk <72447916+heraudk@users.noreply.github.com>
2020-11-12 16:12:51 +0100

    Exercices Phyton

---
+++ test/test.py
@@ -0,0 +1,147 @@
+#a=5
+#print (a)
+#b=8.2 
+#print(a+b)
+
+#a = (b+1)
+#print(a)
+
+#c = 1
+#c = c + 3
+#print(c)
+
+#a, b = 4, 5.2
+#print(a,b)
+
+#c = a
+#a = b
+#b = c
+#print(a,b)
+#print(type(a))
+#print(type(b))
+
+#chaine = """Et voila, du texte"""
+#print(chaine)
+
+#var = 10
+#print(type(var))
+#var = str(var)
+#print(type(var))
+#var = float(var)
+#print(type(var))
+#print(var)
+#var = int(var)
+#print(var)
+
+#reponse = input()
+#age = input("Age : ")
+
+#print(age, reponse)
+
+#age = int(input("Quel est votre age ? "))
+
+#if age > 18:
+    #print("Vous etes majeur")
+
+#print(7>5>1)
+#couleur = input("quelle est ta couleur ?")
+#poids = int(input("Quel est ton poids ?"))
+#if couleur == "rouge":
+    #if poids == "10 kg":
+
+    #    print (couleur, poids)
+   # else:
+       # print(poids)
+#else:
+   # print("la couleur n'est pas rouge")
+
+#if couleur == "rouge" and poids > 10:
+    #print (couleur, poids)
+
+#if couleur == "verte" or poids == 15:
+    #print(couleur, poids)
+#else:
+    #print("la condition est fausse")
+
+#i = 0
+#while i < 10:
+    #i += 1 
+    #if i%2 == 0:
+        #continue
+    #print(i)
+
+
+
+
+
+
+
+
+#table = int(input("Voici une table de multiplication"))
+
+#for x in range (1,11):
+    #print(2, "x", table, " = ",x*table)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#formulaire age:
+#age = int(input("Quel est votre age ? "))
+
+#if age >= 18:
+    #print("Vous etes majeur")
+
+#if age <18:
+    #print("Vous etes mineur")
+
+
+
+
+
+
+#Multiplication avec while:
+#a=int(input("enter table number"))
+#b=10
+#i=1
+#while i<=b:
+    #print(a,"x",i,"=",a*i)
+    #i=i+1
+
+
+
+
+#multiplication avec for/in:
+#def multiplication(valeur, max=10):
+    #for i in range(1, max + 1) :
+        #print(valeur * i, end=" ")
+    #print()
+
+#multiplication (5)
+
+
+
+
